6ef790c79b5eccb900d9def2d1057a58ce726946
riblyet <joaoribeiromachadopedro@gmail.com>
2024-02-12 16:42:17 +0000

    Paginas

--- TPC 1/MapaRuas-materialBase/geraInicio.py
+++ TPC 1/MapaRuas-materialBase/geraInicio.py
@@ -1,5 +1,6 @@
 import os
 import xml.etree.ElementTree as ET 
+from xmldt import XmlDt
 
 
 html = """
@@ -43,7 +44,7 @@ for rua in ruas:
     html += f"""
         <tr>
             <td>
-                <a href="texto/{rua[2]}.html">
+                <a href="htmls/{rua[2]}.html">
                     {rua[1]}
                 </a>
             </td>
@@ -55,4 +56,54 @@ for rua in ruas:
 
 with open('index.html', 'w', encoding='utf-8') as f:
     f.write(html)
-    
+
+
+
+class proc (XmlDt):
+    def número(s, e): 
+        e.tag = "h3" 
+        e.c = "Nº " + e.c 
+        return e.xml
+    def nome(s, e)  :      
+        e.tag = "h1" 
+        return e.xml
+    def corpo(s, e):       
+        e.tag = "body"
+        return e.xml
+    def para(s, e):       
+        e.tag = "p"   
+        return e.xml
+    def lugar(s, e):      
+        e.tag = "b"   
+        return e.xml
+    def lista_casas(s, e): 
+        e.tag = "ul" 
+        return e.xml 
+    def imagem(s, e):     
+        e.tag = "img" 
+        e['src'] = e['path'] 
+        return e.xml
+    def legenda(s, e): 
+        e.tag = "h4"  
+        e.c = "Fig. : "    + e.c 
+        return e.xml
+    def enfiteuta(s, e):  
+        e.tag = "li"  
+        e.c = "Inquilino: " + e.c
+        return e.xml 
+    def foro(s, e):     
+        e.tag = "li"  
+        e.c = "Pensao: "    + e.c
+        return e.xml
+    def desc(s, e):      
+        e.tag = "li" 
+        e.c = "Descrição: " + e.c
+        return e.xml
+
+for file in os.listdir("texto"):
+
+    conteudo = proc(filename = f"texto/{file}")   
+    novo_nome = file[:-3] + "html"
+    with open(f"htmls/{novo_nome}", "w", encoding="utf-8") as f:
+        f.write(conteudo)
+        
